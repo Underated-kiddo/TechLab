7450ba02ba50a6416dc8258ad19ebd7ee6b2e121
Underated-kiddo <lawrencebradley231@gmail.com>
2026-02-14 01:16:19 -0800

    added admindashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
     import { Routes, Route } from "react-router-dom";
     import Home from "./pages/Home";
     import Dashboard from "./pages/Dashboard";
+    import AdminDash from "./pages/AdminDash";
 
     export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/admin-dashboard" element={<AdminDash/>}/>
         </Routes>
     );
     }
